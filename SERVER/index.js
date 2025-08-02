bcd55aaf9a55e36142ccc43374501f7c54de5c0a
M Altaf <maltaf@example.com>
2025-08-03 01:37:08 +0800

    Fix CORS for frontend Render domain

--- SERVER/index.js
+++ SERVER/index.js
@@ -3,8 +3,13 @@ const cors = require("cors");
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+const allowedOrigins = ["https://mern-cra-frontend.onrender.com"];
 
-app.use(cors());
+app.use(
+  cors({
+    origin: allowedOrigins,
+  })
+);
 
 app.get("/api/test", (req, res) => {
   res.json({ message: "Hello from Backend - CRA" });
